7acf55c36b09fe6fa7c5eea0e22f0f89653423eb
TheUnknownCurry <82501161+TheUnknownCurry@users.noreply.github.com>
2021-06-07 18:02:42 +0930

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var database,form,player,game
+var gameState = 0 
+var playerCount
+function setup(){
+    createCanvas(400,400)
+    database = firebase.database()
+    game = new Game()
+    game.getState()
+    game.start()
+}
+function draw(){
+
+}
